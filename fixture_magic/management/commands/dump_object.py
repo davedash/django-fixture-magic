91d9c8a059886379eed4308478c8243db9ad5ab6
Dave Dash <dd+github@davedash.com>
2010-03-08 17:11:20 -0800

    --kitchensink lets you dump all related objects as well.  We now use pk__in instead of id__in

--- fixture_magic/management/commands/dump_object.py
+++ fixture_magic/management/commands/dump_object.py
@@ -1,3 +1,6 @@
+from optparse import make_option
+
+from django.core.exceptions import FieldError
 from django.core.management.base import BaseCommand
 from django.db.models import loading
 from django.core.serializers import serialize
@@ -11,23 +14,56 @@ def get_fields(obj):
         return []
 
 
+serialize_me = []
+seen = {}
+
+def add_to_serialize_list(objs):
+    for obj in objs:
+        if obj is None:
+            continue
+
+        key = "%s:%s:%s" % (obj._meta.app_label, obj._meta.module_name,
+                            obj.pk)
+        if key not in seen:
+            serialize_me.append(obj)
+            seen[key] = 1
+
+
 class Command(BaseCommand):
     help = ('Dump specific objects from the database into JSON that you can '
             'use in a fixture')
     args = "[object_class id ...]"
 
+    option_list = BaseCommand.option_list + (
+            make_option('--kitchensink', action='store', dest='kitchensink',
+                default=False,
+                help='Attempts to get related objects as well.'),
+            )
+
     def handle(self, object_class, *ids, **options):
         (app_label, model_name) = object_class.split('.')
         dump_me = loading.get_model(app_label, model_name)
-        obj = dump_me.objects.filter(id__in=[int(i) for i in ids])
-        serialize_me = list(obj)
+        objs = dump_me.objects.filter(pk__in=[int(i) for i in ids])
+
+        if options.get('kitchensink'):
+            related_fields = [rel.get_accessor_name() for rel in
+                          dump_me._meta.get_all_related_objects()]
+
+            for obj in objs:
+                for rel in related_fields:
+                    try:
+                        add_to_serialize_list(obj.__getattribute__(rel).all())
+                    except FieldError:
+                        pass
+
+        add_to_serialize_list(objs)
         index = 0
 
         while index < len(serialize_me):
             for field in get_fields(serialize_me[index]):
                 if isinstance(field, models.ForeignKey):
-                    serialize_me.append(
-                            serialize_me[index].__getattribute__(field.name))
+                    add_to_serialize_list(
+                        [serialize_me[index].__getattribute__(field.name)])
 
             index = index + 1
 
