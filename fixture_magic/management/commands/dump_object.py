fa7e84a942a279608add93269c245ccc9f242aa0
James Burkhart <jburkhart@gm.slc.edu>
2011-06-06 22:55:42 -0700

    Fixed issue where the optparse.make_option was improperly configured for --kitchensink. Fixed lack of input validation and notification and clarified argument syntax for dump_object.

--- fixture_magic/management/commands/dump_object.py
+++ fixture_magic/management/commands/dump_object.py
@@ -1,9 +1,9 @@
 from optparse import make_option
 
 from django.core.exceptions import FieldError, ObjectDoesNotExist
-from django.core.management.base import BaseCommand
-from django.db.models import loading
+from django.core.management.base import BaseCommand, CommandError
 from django.core.serializers import serialize
+from django.db.models import loading
 
 from fixture_magic.utils import (add_to_serialize_list, serialize_me,
         serialize_fully)
@@ -12,16 +12,31 @@ from fixture_magic.utils import (add_to_serialize_list, serialize_me,
 class Command(BaseCommand):
     help = ('Dump specific objects from the database into JSON that you can '
             'use in a fixture')
-    args = "[object_class id ...]"
+    args = "<[--kitchensink | -k] object_class id1 [id2 [...]]>"
 
     option_list = BaseCommand.option_list + (
-            make_option('--kitchensink', action='store', dest='kitchensink',
+            make_option('--kitchensink', '-k',
+                action='store_true', dest='kitchensink',
                 default=False,
                 help='Attempts to get related objects as well.'),
             )
 
-    def handle(self, object_class, *ids, **options):
-        (app_label, model_name) = object_class.split('.')
+    def handle(self, *args, **options):
+        error_text = ('%s\nTry caling dump_object with --help argument or use'+
+                ' the following arguments:\n %s' %self.args)
+        try:
+            #verify input is valid
+            (app_label, model_name) = args[0].split('.')
+            ids = args[1:]
+            assert(ids)
+        except IndexError:
+            raise CommandError(error_text %'No object_class or id arguments supplied.')
+        except ValueError:
+            raise CommandError(error_text %("object_class must be provided in"+
+                    " the following format: app_name.model_name"))
+        except AssertionError:
+            raise CommandError(error_text %'No id arguments supplied.')
+
         dump_me = loading.get_model(app_label, model_name)
         try:
             objs = dump_me.objects.filter(pk__in=[int(i) for i in ids])
