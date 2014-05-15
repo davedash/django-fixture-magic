18afd0b16c979304325cb9826c819d4f9c922f85
Juanjo <juanjo.arreola@gmail.com>
2014-05-15 17:48:14 -0500

    using --database

--- fixture_magic/management/commands/dump_object.py
+++ fixture_magic/management/commands/dump_object.py
@@ -4,6 +4,7 @@ from django.core.exceptions import FieldError, ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 from django.core.serializers import serialize
 from django.db.models import loading
+from django.db import DEFAULT_DB_ALIAS
 
 from fixture_magic.utils import (add_to_serialize_list, serialize_me,
         serialize_fully)
@@ -19,11 +20,17 @@ class Command(BaseCommand):
                 action='store_true', dest='kitchensink',
                 default=False,
                 help='Attempts to get related objects as well.'),
+            make_option('--database', action='store', dest='database',
+                default=DEFAULT_DB_ALIAS, help='Nominates a specific database to dump '
+                'fixtures from. Defaults to the "default" database.'),
             )
 
     def handle(self, *args, **options):
         error_text = ('%s\nTry calling dump_object with --help argument or ' +
                       'use the following arguments:\n %s' %self.args)
+
+        using = options.get('database')
+
         try:
             #verify input is valid
             (app_label, model_name) = args[0].split('.')
@@ -40,16 +47,16 @@ class Command(BaseCommand):
         dump_me = loading.get_model(app_label, model_name)
         try:
             if ids[0] == '*':
-                objs = dump_me.objects.all()
+                objs = dump_me.objects.using(using).all()
             else:
-                objs = dump_me.objects.filter(pk__in=[int(i) for i in ids])
+                objs = dump_me.objects.using(using).filter(pk__in=[int(i) for i in ids])
         except ValueError:
             # We might have primary keys that are longs...
             try:
-                objs = dump_me.objects.filter(pk__in=[long(i) for i in ids]) 
+                objs = dump_me.objects.using(using).filter(pk__in=[long(i) for i in ids]) 
             except ValueError:
                 # Finally, we might have primary keys that are strings...
-                objs = dump_me.objects.filter(pk__in=ids)
+                objs = dump_me.objects.using(using).filter(pk__in=ids)
 
         if options.get('kitchensink'):
             related_fields = [rel.get_accessor_name() for rel in
@@ -58,13 +65,13 @@ class Command(BaseCommand):
             for obj in objs:
                 for rel in related_fields:
                     try:
-                        add_to_serialize_list(obj.__getattribute__(rel).all())
+                        add_to_serialize_list(obj.__getattribute__(rel).(using).all(), using)
                     except FieldError:
                         pass
                     except ObjectDoesNotExist:
                         pass
 
-        add_to_serialize_list(objs)
+        add_to_serialize_list(objs, using)
         serialize_fully()
         self.stdout.write(serialize('json', [o for o in serialize_me if o is not None],
                 indent=4))
