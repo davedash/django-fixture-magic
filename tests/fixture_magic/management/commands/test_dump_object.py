7348bc7fce18294c1d524dc2c0e3886e87ddbcd8
Dave Dash <dd@davedash.com>
2014-10-23 14:41:34 -0700

    Basic test to prevent this from not being importable.

---
+++ tests/fixture_magic/management/commands/test_dump_object.py
@@ -0,0 +1,10 @@
+__author__ = 'davedash'
+
+
+def test_import():
+    """Just tests that the command can be imported.
+
+    This can be removed entirely once we legitimately test this command.
+    """
+    # noinspection PyUnresolvedReferences
+    from fixture_magic.management.commands import dump_object
