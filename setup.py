5f2a55b016baa168785c260a34d3fb29e32058d0
Dave Dash <dd@davedash.com>
2016-08-11 09:13:51 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-fixture-magic',
-    version='0.1.0',
+    version='0.1.1',
     description='A few extra management tools to handle fixtures.',
     long_description=open('README.rst').read(),
     author='Dave Dash',
