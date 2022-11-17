0056a3389c88e3c6cd97da0ce384c84396d577e0
Andy Zickler <andy@zickler.us>
2022-11-17 09:23:42 -0500

    Update tests to Django 3.2 - 4.1
    * Update tox.ini to use testing matrix
    * Update tox.ini to test Python 3.7, 3.8, 3.9, 3.10
    * Update tox.ini to test Django 3.2, 4.0, 4.1
    * Use Coverage for testing coverage
    * Fix failed UtilsTestCase tests that were broken in #81
    * Create some basic models to use in tests
    * Add __pycache__ to .gitignore

---
+++ tests/fixture_magic/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField(auto_now_add=True)
+    invite_reason = models.CharField(max_length=64)
---
+++ tests/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = "not empty"
+
+INSTALLED_APPS = [
+    "tests",
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
