7b8bcec96a07ff6a4746e6d1c9a60ac5ff8b7735
Andy Zickler <andy@zickler.us>
2023-10-26 07:06:56 -0400

    Remove future and future imports
    We haven't supported python 2 for a while.
    
    * Removes unused future from requirements.txt
    * Removes import of print_function
    
    resolves #87

--- fixture_magic/management/commands/dump_object.py
+++ fixture_magic/management/commands/dump_object.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from django.core.exceptions import FieldError, ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 from django.core.serializers import serialize
